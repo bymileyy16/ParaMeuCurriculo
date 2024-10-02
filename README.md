ce67860d090845af17bdd2437c854b1f902a20d5
Emily <menezesemily2008@gmail.com>
2024-10-02 15:22:03 -0300

    Atividade

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# ParaMeuCurrilo
+## Mega Sena
++ Para começar, Nesse codigo Foi ultilizado a linguagem C#(Csharp)
+
+### Etapas Implementadas
++ I. Primeiro perguntamos ao usuario quantos jogos e dezenas o usuario deseja fazer;
++ II. Se o número de dezenas for 6 ou mais, o programa continua, caso contrário, ele avisa que o mínimo é 6 e repete o pedido.
+ dessa forma fica o codigo em execução:
+![alt text](image.png)
+
++ III. O codigo funciona a partir do **Randow** que gera numeros automaticos, ele e um codigo execencial para geração de codigos
+para numeros de loteria;
++ IV. Dentro deste loop de codigos, uma nova instância de Random é criada para gerar números aleatórios entre 1 e 59;
++ V. 
---
+++ megaSena/Mega Sena/Program.cs
@@ -0,0 +1,37 @@
+﻿
+		bool repetir = false; // Repetição falsa
+		Console.Write("Quantos jogos vc deseja fazer? "); // Impressao na tela
+		int jogadas = int.Parse(Console.ReadLine()); // são usados para converter uma representação de string de um número em um inteiro
+		do
+		{
+			Console.Write("Quantas dezenas vc deseja fazer? "); // Impressao na tela
+			int dezenas = int.Parse(Console.ReadLine()); // são usados para converter uma representação de string de um número em um inteiro
+			if (dezenas >= 6) // Significa para tomar decisões 
+			{
+				repetir = false; // repetição falsa
+				for (int n = 1; n <= jogadas; n++) // Toma decisões
+				{
+					Console.WriteLine($"\nJogo {n}:"); // Grava apresentações de textos
+					for (int contador = 1; contador <= dezenas; contador++)
+					{
+						Random na = new Random(); // Gerador de numero aleatorios
+						int valores = na.Next(01, 60); // armazena valores inteiros, sem casas decimais
+						if (contador == dezenas) // Toma decisões 
+						{
+							Console.Write(valores.ToString("00")); // Impressao na tela
+						}
+						else
+						{
+							Console.Write(valores.ToString("00")); // impressao na tela
+						}
+					}
+				}
+			}
+			else 
+			{
+				repetir = true; // repetição verdadeira
+				Console.WriteLine("O minimo de dezenas e 6!"); // impressao de Tela
+			}
+		}
+		while (repetir == true); // repetição verdadeira
+
